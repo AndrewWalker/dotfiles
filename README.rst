bc8640aff32fa7fa4a4d3678d74f9acd40ccf3ed
Andrew Walker <walker.ab@gmail.com>
2014-02-28 22:17:34 +1100

    initial commit

---
+++ README.rst
@@ -0,0 +1,31 @@
+Personal Dotfiles 
+=================
+
+A basic configuration to bootstrap a machine to make it useful for development 
+
+Requirements
+------------
+
+The following must be installed:
+
+* bash
+* git
+* vim
+* python
+
+Contributions
+-------------
+
+Much of the content here is based on material developed in collaboration with
+(or borrowed from)
+
+* Scott Penrose
+* Brad Dean
+* Daniel Cousens
+* Tim Dubois
+* Samuel Jenkins
+
+License
+-------
+
+MIT License. See `LICENSE.txt <LICENSE.txt>`_.
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# base-path to the location that this script is being run from
+BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# if vundle is not installed
+if [ ! -d ~/.vim/bundle/vundle ]; then
+    # install vundle
+    install -d ~/.vim/bundle
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+    # do an initial provisioning of vim
+    bash -ic 'vim +BundleInstall +qall'
+fi
+
+# link the appropriate files into place
+ln -s -f $BASEPATH/tmux/tmux.conf ~/.tmux.conf
+ln -s -f $BASEPATH/git/gitconfig ~/.gitconfig
+ln -s -f $BASEPATH/git/gitignore ~/.gitignore
+ln -s -f $BASEPATH/vim/vimrc ~/.vimrc
+ln -s -f $BASEPATH/bash/bashrc.common ~/.bashrc.common
+mkdir -p ~/.pip/
+ln -s -f $BASEPATH/pip/pip.conf ~/.pip/pip.conf
+
+if grep -Fxq "source ~/.bashrc.common" ~/.bashrc 
+then    
+    echo "bash common already installed"
+else
+    echo "source ~/.bashrc.common" >> ~/.bashrc
+fi
