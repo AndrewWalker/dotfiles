dcb27f51a4ef22b952a0b81431efb9623e76fdb4
Andrew Walker <walker.ab@gmail.com>
2015-02-11 12:17:03 +0000

    refactoring python code

--- install.sh
+++ install.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-function installpip {
-    which pip > /dev/null
-    if [[ "$?" != "0" ]]; then
-        wget -c https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-        python get-pip.py --user
-    fi
-}
-
 sudo apt-get install \
     python-dev \
     wget \
     libpng12-dev \
     libfreetype6-dev
-
-# we need to temporarily add this so that if this is happening for the
-# very first time we can find pip and virtualenv even without a bashrc
-# file
-PATH=$PATH:~/.local/bin
-
-rm -f ~/.pip/pip.conf
-
-installpip
-pip install --upgrade pip wheel virtualenvwrapper --user
 
 # base-path to the location that this script is being run from
 BASEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -40,11 +22,6 @@ ln -s -f $BASEPATH/git/gitconfig ~/.gitconfig
 ln -s -f $BASEPATH/git/gitignore ~/.gitignore
 ln -s -f $BASEPATH/vim/vimrc ~/.vimrc
 ln -s -f $BASEPATH/bash/bashrc.common ~/.bashrc.common
-mkdir -p ~/.pip/
-ln -s -f $BASEPATH/pip/pip.conf ~/.pip/pip.conf
-
-pip wheel -r python/requirements.txt
-pip install -r python/requirements.txt --user
 
 
 if grep -Fxq "source ~/.bashrc.common" ~/.bashrc 
